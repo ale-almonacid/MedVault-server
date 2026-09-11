5046b6bbefc9cd10da39c138b5d4f457db712d9e
Alejandra Almonacid <alejandraalmonacidayala@gmail.com>
2026-09-11 20:37:31 +0200

    API routes Share MedicalProfile

--- routes/medicalProfiles.routes.js
+++ routes/medicalProfiles.routes.js
@@ -1,158 +1,286 @@
-const express = require("express")
+const express = require("express");
 const router = require("express").Router();
 
-//model
+//Models
 const MedicalProfile = require("../models/MedicalProfile.model");
+const User = require("../models/User.model");
 
 // Authentication middleware
 const { verifyToken } = require("../middleware/auth.middleware");
 
-//Routes
+//------------------ Routes -------------------------
 
 // GET all medical profiles of a user (/api/medical-profiles)
-router.get("/", verifyToken, async (req, res, next) =>{
-
-    try {
-        const userId = req.payload._id;   
-        const response = await MedicalProfile.find({
-
-            $or:[{owner: userId },{ "sharedWith.user": userId }] 
-        })
-        res.status(200).json(response)  
-    }   catch (error) {
-        next (error)
-    }
-})
+router.get("/", verifyToken, async (req, res, next) => {
+  try {
+    const userId = req.payload._id;
+    const response = await MedicalProfile.find({
+      $or: [{ owner: userId }, { "sharedWith.user": userId }],
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // GET 1 medical profile details (/api/medical-profiles/:medicalProfileId)
 
-router.get("/:medicalProfileId", verifyToken, async (req, res, next) =>{
-
-    try {
-        const userId = req.payload._id;   
-        const response = await MedicalProfile.findOne({
-            _id: req.params.medicalProfileId, // explicit id is better 
-            $or:[{owner: userId },{ "sharedWith.user": userId }] 
-        })
-        res.status(200).json(response)  
-    }   catch (error) {
-        next (error)
-    }
-})
+router.get("/:medicalProfileId", verifyToken, async (req, res, next) => {
+  try {
+    const userId = req.payload._id;
+    const response = await MedicalProfile.findOne({
+      _id: req.params.medicalProfileId, // explicit id is better
+      $or: [{ owner: userId }, { "sharedWith.user": userId }],
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // POST Create a medical profile (/api/medical-profiles)
 
-router.post("/", verifyToken, async (req, res, next ) =>{
-
-    try {
-       const userId = req.payload._id;
-       const {subjectName, description, categories, sharedWith} = req.body  // no need to destructure owner because here is only for the user editable fields 
-       const response = await MedicalProfile.create({
-        subjectName,
-        description,
-        owner: userId, // Force owner to be the authenticated user
-        categories, 
-        sharedWith
-       })
-       res.status(201).json(response); 
-    } catch (error) {
-        next (error)
-    }
-
-
-
-})
+router.post("/", verifyToken, async (req, res, next) => {
+  try {
+    const userId = req.payload._id;
+    const { subjectName, description, categories, sharedWith } = req.body; // no need to destructure owner because here is only for the user editable fields
+    const response = await MedicalProfile.create({
+      subjectName,
+      description,
+      owner: userId, // Force owner to be the authenticated user
+      categories,
+      sharedWith,
+    });
+    res.status(201).json(response);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // PATCH Edit Basic Profile Info (Name & Description) (/api/medical-profiles/:medicalProfileId)
 
-router.patch("/:medicalProfileId", verifyToken, async (req, res, next) =>{
+router.patch("/:medicalProfileId", verifyToken, async (req, res, next) => {
+  try {
+    const userId = req.payload._id;
+    const { medicalProfileId } = req.params;
+    const { subjectName, description } = req.body;
 
-    try {
-        const userId = req.payload._id;
-        const { medicalProfileId } = req.params;
-        const {subjectName, description} = req.body  
+    const response = await MedicalProfile.findOneAndUpdate(
+      {
+        _id: medicalProfileId,
+        $or: [
+          { owner: userId },
+          {
+            sharedWith: { $elemMatch: { user: userId, permission: "editor" } },
+          },
+        ],
+      },
+      {
+        subjectName,
+        description,
+      },
+      {
+        runValidators: true,
+        returnDocument: "after",
+      },
+    );
 
-        const response = await MedicalProfile.findOneAndUpdate({
-            _id: medicalProfileId,
-            $or:[{owner: userId},{sharedWith:{ $elemMatch: { user: userId, permission: "editor" }}}],
-        },{
-            subjectName,
-            description,
-        },{
-            runValidators: true, 
-            returnDocument: "after"
-        })
-        
-        if(!response){
-           res.status(404).json({ message: "Medical profile not found or permission denied." })   
-           return
-        }
-
-        res.status(200).json(response)  
-
-
-    }   catch (error) {
-        next (error)
+    if (!response) {
+      res
+        .status(404)
+        .json({ message: "Medical profile not found or permission denied." });
+      return;
     }
-})
+
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
+});
 
 // PATCH Edit (add and remove) categories of profile (/api/medical-profiles/:medicalProfileId/categories)
 
-router.patch("/:medicalProfileId/categories", verifyToken, async (req, res, next) =>{
-
+router.patch(
+  "/:medicalProfileId/categories",
+  verifyToken,
+  async (req, res, next) => {
     try {
-        const userId = req.payload._id;
-        const { medicalProfileId } = req.params;
-        const {categories} = req.body  
+      const userId = req.payload._id;
+      const { medicalProfileId } = req.params;
+      const { categories } = req.body;
 
-        const response = await MedicalProfile.findOneAndUpdate({
-            _id: medicalProfileId,
-            $or:[{owner: userId},{sharedWith:{ $elemMatch: { user: userId, permission: "editor" }}}],
-        },{
-            categories,
-        },{
-            runValidators: true, 
-            returnDocument: "after"
-        })
-        
-        if(!response){
-           res.status(404).json({ message: "Medical profile not found or permission denied." })   
-           return
-        }
+      const response = await MedicalProfile.findOneAndUpdate(
+        {
+          _id: medicalProfileId,
+          $or: [
+            { owner: userId },
+            {
+              sharedWith: {
+                $elemMatch: { user: userId, permission: "editor" },
+              },
+            },
+          ],
+        },
+        {
+          categories,
+        },
+        {
+          runValidators: true,
+          returnDocument: "after",
+        },
+      );
 
-        res.status(200).json(response)  
+      if (!response) {
+        res
+          .status(404)
+          .json({ message: "Medical profile not found or permission denied." });
+        return;
+      }
 
-
-    }   catch (error) {
-        next (error)
+      res.status(200).json(response);
+    } catch (error) {
+      next(error);
     }
-})
+  },
+);
 
 //DELETE  Delete a medical profile (/api/medical-profiles/:medicalProfileId)
 
-router.delete("/:medicalProfileId", verifyToken,  async (req, res, next ) =>{
-   
-    try {
-        const userId = req.payload._id;   
-        const response = await MedicalProfile.findOneAndDelete({
-            _id: req.params.medicalProfileId, 
-            $or:[{owner: userId},{sharedWith:{ $elemMatch: { user: userId, permission: "editor" }}}] 
-        })
+router.delete("/:medicalProfileId", verifyToken, async (req, res, next) => {
+  try {
+    const userId = req.payload._id;
+    const response = await MedicalProfile.findOneAndDelete({
+      _id: req.params.medicalProfileId,
+      $or: [
+        { owner: userId },
+        { sharedWith: { $elemMatch: { user: userId, permission: "editor" } } },
+      ],
+    });
 
-        if (!response) {
-        return res.status(404).json({ message: "Medical profile not found or permission denied." });
-        }
-
-        res.status(200).json({ message: "Medical profile deleted successfully." })  
-    }   catch (error) {
-        next (error)
+    if (!response) {
+      return res
+        .status(404)
+        .json({ message: "Medical profile not found or permission denied." });
     }
-    
-})
 
+    res.status(200).json({ message: "Medical profile deleted successfully." });
+  } catch (error) {
+    next(error);
+  }
+});
+
+//-------------------------------
+
+// Routes share medical profile
+
+//---------------------------------
+
+//POST Add / Grant Profile Permission (/api/medical-profiles/:medicalProfileId/share)
+
+router.post("/:medicalProfileId/share", verifyToken, async (req, res, next) => {
+  try {
+    const userId = req.payload._id;
+    const { medicalProfileId } = req.params;
+    const { emailToShareWith, permission } = req.body;
+
+
+    // Find the user by email to get their _id
+    const userToShareWith = await User.findOne({ email: emailToShareWith });
+    if (!userToShareWith) {
+      return res.status(404).json({ message: "User with that email does not exist." });
+    }
+
+    const response = await MedicalProfile.findOneAndUpdate(
+      {
+        _id: medicalProfileId,
+        "sharedWith.user": { $ne: userToShareWith._id}, // <--- Blocks duplicate users!
+        $or: [
+          { owner: userId },
+          {sharedWith: { $elemMatch: { user: userId, permission: "editor" } },},
+        ],
+      },
+      {$push: { sharedWith: { user: userToShareWith._id, permission } },},
+      { runValidators: true, returnDocument: "after" },
+    );
+
+    if (!response) {
+      return res
+        .status(404)
+        .json({ message: "Medical profile not found or permission denied." });
+    }
+    res.status(200).json({ message: "permission granted", profile: response });
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+//PATCH Update Delegate Permission (/api/medical-profiles/:medicalProfileId/share/:recipientUserId)
+
+router.patch("/:medicalProfileId/share/:recipientUserId", verifyToken, async (req, res, next) => {
+  try {
+    const userId = req.payload._id;
+    const { medicalProfileId, recipientUserId } = req.params;
+    const { permission } = req.body;
+
+    const response = await MedicalProfile.findOneAndUpdate(
+      {
+        _id: medicalProfileId,
+        "sharedWith.user": recipientUserId, // <--- Blocks duplicate users!
+        $or: [
+          { owner: userId },
+          {sharedWith: { $elemMatch: { user: userId, permission: "editor" } },},
+        ],
+      },
+      {$set: { "sharedWith.$.permission": permission }},
+      { runValidators: true, returnDocument: "after" },
+    );
+
+    if (!response) {
+      return res
+        .status(404)
+        .json({ message: "Medical profile or shared user not found not found, or permission denied." });
+    }
+    res.status(200).json({ message: "permission updated" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+//DELETE Revoke Delegate Access(/api/medical-profiles/:medicalProfileId/share/:recipientUserId)
+
+router.delete("/:medicalProfileId/share/:recipientUserId", verifyToken, async (req, res, next) => {
+  try {
+    const userId = req.payload._id;
+    const { medicalProfileId, recipientUserId } = req.params;
+    
+
+    const response = await MedicalProfile.findOneAndUpdate(
+        {
+        _id: medicalProfileId,
+        $or: [
+          { owner: userId },
+          {sharedWith: { $elemMatch: { user: userId, permission: "editor" } },},
+        ],
+      },
+      {$pull: { sharedWith: { user: recipientUserId }},},
+      { runValidators: true, returnDocument: "after" },
+    );
+
+    if (!response) {
+      return res
+        .status(404)
+        .json({ message: "Medical profile not found or permission denied." });
+    }
+    res.status(200).json({ message: "Access revoked successfully", profile: response });
+  } catch (error) {
+    next(error);
+  }
+});
 
 
 
 
 // export
-module.exports = router 
+module.exports = router;
